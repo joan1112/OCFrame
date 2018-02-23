72c8a9a2e3b793e9ed27c80bc49fdc4dc357dfef
joan <1640520382@qq.com>
2018-02-23 09:48:18 +0800

    Initial Commit

---
+++ SiJiOAProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SiJiOAProject
+//
+//  Created by qiong on 2018/2/23.
+//  Copyright © 2018年 qiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SiJiOAProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SiJiOAProject
+//
+//  Created by qiong on 2018/2/23.
+//  Copyright © 2018年 qiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
